39f29130236e305e9a933f4c1c9c835c3d568f07
Danielle Hare <danielle.hare@uconn.edu>
2019-10-29 22:10:17 -0400

    Updated plot script to generate a facet plot (added to folder) as well as single dataframe for each dam purpose (no hierarchy).

--- scripts/DamPurpose_PlotFigures.R
+++ scripts/DamPurpose_PlotFigures.R
@@ -2,10 +2,13 @@
 ## Danielle Hare
 ## October 2019
 ### Advanced Stream Ecology - Hot Dam
+## for later use <- https://stackoverflow.com/questions/21414847/convert-a-date-vector-into-julian-day-in-r
+
 
 # DP - Dam Purpose
 ## Library
 library(plyr) # for rbind.fill
+library(ggplot2)
 
 ##List files that may need to be read
 DP_files <- list.files("input/DamPurposes_LocFiles", recursive= TRUE, full.names =  TRUE)
@@ -33,5 +36,36 @@ for (DP in DP_files) {
       DF <- rbind.fill(DF,df)
     }
   }
+  ### --- Output Dam Purpose Data Frame
+  output_csv <- basename(DP)
+  fp <-  file.path('input/DamPurposes_Dataframes',output_csv)
+  write.csv(DF, file = fp )
+
+
+  
+  ## --- Plot Image for each type *RAW*
+  
+  #make iterative file name
+  output_png <- paste0((sub("^([^.]*).*", "\\1", output_csv)),".png")
+  fp <-  file.path('input/DamPurposes_Dataframes',output_png)
+  
+  # plot
+  try(
+  plot.DP <- ggplot(DF, aes(Date, Wtemp, colour = site_no, group = site_no)) +
+    facet_wrap( ~ site_no, scales = "free")+
+    geom_point(size = 0.5) +
+    geom_smooth(span = 0.3)  
+  )
+  try(
+    ggsave(fp)
+  )
+  
+
 }
+
+
+
+  
+  
+  
   
