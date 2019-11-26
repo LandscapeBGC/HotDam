3e5bd66867ca9d7b73b4377b387d967bd8000d61
Kevin E Jackson <kevin.jackson@uconn.edu>
2019-11-26 17:43:09 -0500

    analysis script that identifies reference gauges

---
+++ scripts/analysis_11262019.R
@@ -0,0 +1,287 @@
+#install.packages("here")
+#install.packages('data.table')
+#install.packages('tidyverse')
+#install.packages('broom')
+#install.packages('dplyr')
+library(here)
+library(dplyr)
+library(ggplot2)
+library(broom)
+library(stats)
+library(tidyverse)
+library(data.table)
+
+#######################################
+start_time <- Sys.time()
+#######################################
+
+path1 <- here()
+Orig_file <- read.csv(file.path(path1,'NWIS_NOAA_NID_Eco_clean.csv'))
+
+path2 <- file.path(here(), 'Thermal_metrics_11262019.csv') # make copy of NWIS_NOAA_NID_Eco_clean.csv that can be edited
+write.csv(Orig_file, file = path2)
+
+Main_df <- read.csv(file.path(path1,'Thermal_metrics_11262019.csv'))
+
+#######################################
+#### Create Thermal Metric Columns #### 
+#######################################
+
+Main_df$pstRcrdCnt <- NaN
+Main_df$pstTmax <- NaN
+Main_df$pstTmin <- NaN
+Main_df$pstTmean <- NaN
+Main_df$pstTmaxT <- NaN
+Main_df$pstTminT <- NaN
+
+Main_df$preRcrdCnt <- NaN
+Main_df$preTmax <- NaN
+Main_df$preTmin <- NaN
+Main_df$preTmean <- NaN
+Main_df$preTmaxT <- NaN
+Main_df$preTminT <- NaN
+
+Main_df$NOAA_TmaxT <- NaN
+Main_df$NOAA_TminT <- NaN
+Main_df$Lag_TmaxT <- NaN
+Main_df$Lag_TminT <- NaN
+Main_df$NOAARcrdCn <- NaN
+
+
+#########################################################
+#### Create Path + List of Post-Construction Records ####
+#########################################################
+
+path3 <- here('input/Dam_thermal_regimes_post')
+pstDam <- dir(path = path3, pattern = "*.csv")
+pstDam_lst <- str_sub(pstDam, end = -8)
+pstUSGs_St_num <- as.list(pstDam_lst) # create an iterable list of USGS gauges with post construction temperature data
+
+##########################################################################################
+#### Calcuate Thermal Metrics of Post-Contruction Records and populate main dataframe ####
+##########################################################################################
+
+NWIS_Str<- str_split(Main_df$STAID_edt, pattern = ",")
+NWIS_Str <- str_split(str_sub(NWIS_Str, start = 6), pattern = ",")
+
+for (n in 1:length(NWIS_Str)){
+  if (NWIS_Str[n] %in% pstUSGs_St_num == TRUE) {
+    tryCatch(
+      expr = {
+        x <- c(file.path(here('input/Dam_thermal_regimes_post/xyxyxyxy_tr.csv')))
+        NWISsubstitution <- gsub("xyxyxyxy",NWIS_Str[n], x)
+        print(NWISsubstitution)
+        analysis.df <- read.csv(NWISsubstitution)
+        tmax <- max(analysis.df$y)
+        print(tmax)
+        tmin <- min(analysis.df$y)
+        tmean <- mean(analysis.df$y)
+        tmax_timing <- analysis.df$x[which.max(analysis.df$y)]
+        tmin_timing <- analysis.df$x[which.min(analysis.df$y)]
+        RcrdCnt <- analysis.df$RcrdCnt[1]
+        print(RcrdCnt)
+        for (z in 1:nrow(Main_df)){
+          if (NWIS_Str[n] == str_sub(Main_df$STAID_edt[z], start = 6)){
+            Main_df$pstRcrdCnt
+            Main_df$pstTmax[z] <- tmax
+            Main_df$pstTmin[z] <- tmin
+            Main_df$pstTmean[z] <- tmean
+            Main_df$pstTmaxT[z] <- tmax_timing
+            Main_df$pstTminT[z] <- tmin_timing
+            Main_df$pstRcrdCnt[z] <- RcrdCnt
+          }}},error = function(cond){
+            message(paste0(NWIS_Str[n]))
+            return(NA)})}}
+
+########################################################
+#### Create Path + List of Pre-Construction Records ####
+########################################################
+
+path4 <- here('input/Dam_thermal_regimes_pre')
+preDam <- dir(path = path4, pattern = "*.csv")
+preDam_lst <- str_sub(preDam, end = -8)
+preUSGs_St_num <- as.list(preDam_lst) # create an iterable list of USGS gauges with post construction temperature data
+
+#########################################################################################
+#### Calcuate Thermal Metrics of Pre-Contruction Records and populate main dataframe ####
+#########################################################################################
+
+NWIS_Str<- str_split(Main_df$STAID_edt, pattern = ",")
+NWIS_Str <- str_split(str_sub(NWIS_Str, start = 6), pattern = ",")
+
+for (n in 1:length(NWIS_Str)){
+  if (NWIS_Str[n] %in% preUSGs_St_num == TRUE) {
+    tryCatch(
+      expr = {
+        x <- c(file.path(here('input/Dam_thermal_regimes_pre/xyxyxyxy_tr.csv')))
+        NWISsubstitution <- gsub("xyxyxyxy",NWIS_Str[n], x)
+        print(NWISsubstitution)
+        analysis.df <- read.csv(NWISsubstitution)
+        tmax <- max(analysis.df$y)
+        print(tmax)
+        tmin <- min(analysis.df$y)
+        tmean <- mean(analysis.df$y)
+        tmax_timing <- analysis.df$x[which.max(analysis.df$y)]
+        tmin_timing <- analysis.df$x[which.min(analysis.df$y)]
+        RcrdCnt <- analysis.df$RcrdCnt[1]
+        print(RcrdCnt)
+        for (z in 1:nrow(Main_df)){
+          if (NWIS_Str[n] == str_sub(Main_df$STAID_edt[z], start = 6)){
+            Main_df$preTmax[z] <- tmax
+            Main_df$preTmin[z] <- tmin
+            Main_df$preTmean[z] <- tmean
+            Main_df$preTmaxT[z] <- tmax_timing
+            Main_df$preTminT[z] <- tmin_timing
+            Main_df$preRcrdCnt[z] <- RcrdCnt
+          }}},error = function(cond){
+            message(paste0(NWIS_Str[n]))
+            return(NA)})}}
+
+########################################################
+#### Create Path + List of Pre-Construction Records ####
+########################################################
+
+path5 <- here('input/NOAA_thermal_regimes')
+NOAA <- dir(path = path5, pattern = "*.csv")
+NOAA_lst <- str_sub(NOAA, end = -5)
+NOAA_St_num <- as.list(NOAA_lst) # create an iterable list of USGS gauges with post construction temperature data
+
+##############################################################
+#### Calcuate Julian Date of NOAA populate main dataframe ####
+##############################################################
+
+NOAA_Str<- str_split(Main_df$NOAA_ID, pattern = ",")
+NOAA_Str <- str_split(str_sub(NOAA_Str), pattern = ",")
+
+tryCatch(
+  expre = {
+    for (n in 1:length(NOAA_Str)) {
+      for (y in 1:length(NOAA_St_num)){
+        tryCatch(
+          expr = {
+            if (NOAA_Str[[n]] == NOAA_St_num[[y]]){
+              tryCatch(
+                expr = {
+                  x <- c(file.path(here('input/NOAA_thermal_regimes/xyxyxyxy.csv')))
+                  NOAAsubstitution <- gsub("xyxyxyxy",NOAA_St_num[y], x)
+                  print(NOAAsubstitution)
+                  analysis.df <- read.csv(NOAAsubstitution)
+                  tmax <- max(analysis.df$y)
+                  tmin <- min(analysis.df$y)
+                  tmax_timing <- analysis.df$x[which.max(analysis.df$y)]
+                  tmin_timing <- analysis.df$x[which.min(analysis.df$y)]
+                  RcrdCnt <- analysis.df$pstRcrdCnt[1]
+                  Main_df$NOAA_TmaxT[n] <- tmax_timing
+                  Main_df$NOAA_TminT[n] <- tmin_timing
+                  
+                  tmax_tm_diff <- abs(Main_df$pstTmaxT[n] - tmax_timing)
+                  if (tmax_tm_diff > 180){
+                    if (tmax_timing < Main_df$pstTmaxT[n]){
+                      tmax_timing = tmax_timing + 365
+                    }
+                    else {
+                      tmax_timing = tmax_timing - 365
+                    }
+                  }
+                  Main_df$Lag_TmaxT[n] <-  abs(Main_df$pstTmaxT[n] - tmax_timing)
+                  
+                  tmin_tm_diff <- abs(Main_df$pstTminT[n] - tmin_timing)
+                  if (tmin_tm_diff > 180){
+                    if (tmin_timing < Main_df$pstTminT[n]){
+                      tmin_timing = tmin_timing + 365
+                    }
+                    else {
+                      tmin_timing = tmin_timing - 365
+                    }
+                  }
+                  Main_df$Lag_TminT[n] <- abs(Main_df$pstTminT[n] - tmin_timing)
+                  
+                  Main_df$NOAARcrdCn[n] <- RcrdCnt
+                }
+                ,error = function(cond){
+                  message(paste0(NOAA_St_num[[y]]))
+                  return(NA)
+                }
+              )
+            }
+          }
+          ,error = function(cond){
+            message(paste0(NOAA_Str[[n]]))
+            return(NA)
+          }
+        )
+      }
+    }
+  }
+  ,error = function(cond){
+    message(paste0(NOAA_Str[[n]]))
+    return(NA)
+  }
+)
+
+#################################################
+#### Create Path + List of Reference Records ####
+#################################################
+
+path6 <- here('input/Ref_thermal_regimes')
+Rfrnc <- dir(path = path6, pattern = "*.csv")
+Rfrnc_lst <- str_sub(Rfrnc, end = -8)
+Rfrnc_St_num <- as.list(Rfrnc_lst) 
+
+###################################################################################
+#### Calcuate Thermal Metrics of Reference Records and populate main dataframe ####
+###################################################################################
+
+NWIS_Str<- str_split(Main_df$STAID_edt, pattern = ",")
+NWIS_Str <- str_split(str_sub(NWIS_Str, start = 6), pattern = ",")
+
+Lngth = o
+for (n in 1:length(NWIS_Str)){
+  if (NWIS_Str[n] %in% Rfrnc_St_num == TRUE) {
+    Lngth = Lngth+1}}
+print(Lngth)
+
+Lngth=0
+for (n in 1:length(NWIS_Str)){
+  if (NWIS_Str[n] %in% Rfrnc_St_num == TRUE) {
+    tryCatch(
+      expr = {
+        x <- c(file.path(here('input/Ref_thermal_regimes/xyxyxyxy_tr.csv')))
+        NWISsubstitution <- gsub("xyxyxyxy",NWIS_Str[n], x)
+        print(NWISsubstitution)
+        analysis.df <- read.csv(NWISsubstitution)
+        tmax <- max(analysis.df$y)
+        tmin <- min(analysis.df$y)
+        tmean <- mean(analysis.df$y)
+        tmax_timing <- analysis.df$x[which.max(analysis.df$y)]
+        tmin_timing <- analysis.df$x[which.min(analysis.df$y)]
+        RcrdCnt <- analysis.df$pstRcrdCnt[1]
+        print(RcrdCnt)
+        for (z in 1:nrow(Main_df)){
+          if (NWIS_Str[n] == str_sub(Main_df$STAID_edt[z], start = 6)){
+            Main_df$Ref[z] <- '1'
+            Main_df$pstTmax[z] <- tmax
+            Main_df$pstTmin[z] <- tmin
+            Main_df$pstTmaxT[z] <- tmax_timing
+            Main_df$pstTminT[z] <- tmin_timing
+            Main_df$pstTmean[z] <- tmean
+            Main_df$pstRcrdCnt[z] <- RcrdCnt
+            Lngth=Lngth+1
+          }}},error = function(cond){
+            message(paste0(NWIS_Str[n]))
+            return(NA)})}
+  else{
+    Main_df$Ref[n] <- '0'
+  }
+}
+
+
+
+#######################################
+write.csv(Main_df, path2)
+#######################################
+end_time <- Sys.time()
+time <- end_time - start_time
+print(time)
+print("Script Complete")
+#######################################
