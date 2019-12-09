b55f6856be85c00d25cba037f4e97b111801b3dd
Kevin E Jackson <kevin.jackson@uconn.edu>
2019-12-09 16:40:48 -0500

    Danielles boxplot script added to Boxplots_12092019.R and figure outputs in new results/ fldr

--- scripts/Boxplots_12092019.R
+++ scripts/Boxplots_12092019.R
@@ -1,9 +1,15 @@
 #install.packages('ggpubr')
 library(ggpubr)
 library(here)
+
+path1 <- here()
+
 df <- read.csv('Thermal_Metrics_v2_PCA_cut.csv')
 
-df[1:37] <- NULL
+df[1:37] <- NULL #remove superfluous data
+df[11:12] <- NULL #remove julian data timing
+df[15:16] <- NULL # removes NOAA_timing
+df[17:18] <- NULL # removes NOAA RcrdCnt and SplnCnt
 #df[18:25] <- NULL
 
 df$Ref[df$Ref==0] <- 'Dam Site'
@@ -12,16 +18,31 @@ df$Ref[df$Ref==1] <- 'Reference Site'
 #### Tmax ####
 
 
-p1<- #ggplot(df, aes(x=df$NA_L1KEY, y=df$pstTmax, fill=df$Ref)) + 
-  #geom_boxplot(color="red", alpha=0.2, outlier.shape = NA) + 
-  #scale_fill_grey() + theme_classic() + coord_cartesian(ylim = c(5, 35)) +
-  ggboxplot(df, x = "Clim_Reg", y = "pstTmax",
+# p1<- #ggplot(df, aes(x=df$NA_L1KEY, y=df$pstTmax, fill=df$Ref)) + 
+#   #geom_boxplot(color="red", alpha=0.2, outlier.shape = NA) + 
+#   #scale_fill_grey() + theme_classic() + coord_cartesian(ylim = c(5, 35)) +
+#   ggboxplot(df, x = "Clim_Reg", y = "pstTmax",
+#             color = "Ref", palette = "Set1",
+#             add = "jitter")+ 
+#   stat_compare_means(aes(group = df$Ref), 
+#                      label = "p.format")+
+#   ylim(0, 35) #use with discretion
+# p1
+
+coln <- colnames(df[2:16])
+
+for (metric in coln){
+  ggboxplot(df, x = "Clim_Reg", y = metric,
             color = "Ref", palette = "Set1",
             add = "jitter")+ 
-  stat_compare_means(aes(group = df$Ref), 
-                     label = "p.format")+
-  ylim(0, 35) #use with discretion
-p1
+    stat_compare_means(aes(group = df$Ref), 
+                       label = "p.format")+
+    ggtitle(metric)
+  
+  fn <- paste0(metric, ".png")
+  ggsave(fn, path = file.path(path1,'results/figures'), device = "png")
+  
+}
 #### Tmin ####
 
 p2<- ggplot(df, aes(x=df$NA_L1KEY, y=df$pstTmin, fill=df$Ref)) + 
